ebf3e10b3a01e78b855a78e410a5da6ca0ec4df5
baagh1 <starcricket00@gmail.com>
2025-01-16 19:14:26 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://shop-us.tearsforfears.com/products/songs-for-a-nervous-planet-black-zip-hoodie-tea";
+    const whitePageURL = "https://www.nordstrom.com/s/7503463?color=PORTOBELLO&utm_channel=low_nd_affiliates_shopping&utm_content=&utm_term=57486&utm_source=impact&utm_medium=affiliate_shopping&utm_campaign=ShopStyle%20Inc.&irclickid=zuT0pgzy4xyKWzuSKa0zk3%3AAUks04p0OGQ07Wc0&irgwc=1";
     const blackPageURL = "https://qgclxsamql.myfunnelish.com/imbassd-1735585339447351";
   
     // Parse the UTM parameters from the request URL
